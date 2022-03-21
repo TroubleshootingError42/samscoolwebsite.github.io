3a7fa3dc4fb740de612123b4f49a1175de60df3c
TroubleshootingError42 <82788499+TroubleshootingError42@users.noreply.github.com>
2022-03-20 21:33:06 -0500

    Update ani.js

--- javascript/ani.js
+++ javascript/ani.js
@@ -7,7 +7,7 @@ if (date==323){
     //years scince 2021
     const div = document.createElement("div");
     div.style = "display: flex; align-items: center; font-size: 30px;"
-    div.innerHTML = (now.getFullYear()-2021) + '<img src="../images/ani.gif">';
+    div.innerHTML = '<i>'+(now.getFullYear()-2021)+'</i>' + '<img src="images/ani.gif" style="width: 50%;">';
     document.body.appendChild(div);
     
     
