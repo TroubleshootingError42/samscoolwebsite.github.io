36f95fd6e6fd2cc4d2a7eedead600416402901de
TroubleshootingError42 <82788499+TroubleshootingError42@users.noreply.github.com>
2022-03-20 21:19:14 -0500

    Update ani.js

--- javascript/ani.js
+++ javascript/ani.js
@@ -7,7 +7,7 @@ if (date==323){
     //years scince 2021
     const div = document.createElement("div");
     div.style = "display: flex; align-items: center; font-size: 30px;"
-    div.innerHTML = (now.getFullYear()-2021) + '<img src="https://samscoolwebsite.com/images/logo.gif">';
+    div.innerHTML = (now.getFullYear()-2021) + '<img src="../images/ani.gif">';
     document.body.appendChild(div);
     
     
