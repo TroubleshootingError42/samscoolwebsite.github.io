7be54a53e95fcc757ed501b6a038cc512e03af6d
TroubleshootingError42 <82788499+TroubleshootingError42@users.noreply.github.com>
2022-05-05 10:34:42 -0500

    Update ani.js

--- javascript/ani.js
+++ javascript/ani.js
@@ -5,10 +5,10 @@ var date = (now.getMonth()+1)+''+now.getDate();
 if (date==323){
 
     //years scince 2021
-    const div = document.createElement("div");
-    div.style = "display: flex; align-items: center; font-size: 30px;"
-    div.innerHTML = '<i>'+(now.getFullYear()-2021)+'</i>' + '<img src="images/ani.gif" style="width: 50%;">';
-    document.body.appendChild(div);
+    const ani = document.createElement("div");
+    ani.style = "display: flex; align-items: center; font-size: 30px;"
+    ani.innerHTML = '<i>'+(now.getFullYear()-2021)+'</i>' + '<img src="images/ani.gif" style="width: 50%;">';
+    document.body.appendChild(ani);
     
     
 }
