55232e1dcad34ef7a51bac59f20ac720479c85f6
TroubleshootingError42 <82788499+TroubleshootingError42@users.noreply.github.com>
2022-06-14 10:05:14 -0500

    Update splash.js

--- javascript/index/splash.js
+++ javascript/index/splash.js
@@ -1,36 +1,36 @@
 window.onload = function() {
-    var splashList = [
-        "<i>Mindcraft?</i> No no no no no, I will not endulge!",
-        "r/beigejeans, that's a nice place",
-        "Born and raised",
-        "I might hold on to that ip adress of yours",
-        "The Js? Never heard of them.",
-        "Famous rapper emcee MF DOOM",
-        "<div onmouseover='up()'>hey, hover over me</div>",
-        "it's being delivered to you right now!",
-        "Charnlie Brownd"
-    ];
+  var splashList = [
+      "<i>Mindcraft?</i> No no no no no, I will not endulge!",
+      "r/beigejeans, that's a nice place",
+      "Born and raised",
+      "I might hold on to that ip adress of yours",
+      "The Js? Never heard of them.",
+      "Famous rapper emcee MF DOOM",
+      "<div onmouseover='up()'>hey, hover over me</div>",
+      "it's being delivered to you right now!",
+      "Charnlie Brownd",
+      "Due to inflation, sam's cool texture pack is now even more free"
+  ];
 
-    document.getElementById("splash").innerHTML = splashList[Math.floor(Math.random() * splashList.length)];
+  document.getElementById("splash").innerHTML = splashList[Math.floor(Math.random() * splashList.length)];
 };
 
 
 //splash functions
 var splashRight = 0;
 function up() {
-    document.getElementById("splash").innerHTML = "i am free";
-    hovMove1()
+  document.getElementById("splash").innerHTML = "i am free";
+  hovMove1()
 }
 
 function hovMove1() {
-    splashRight += 7
-    document.getElementById("splash").style.right = splashRight+"px"
-  setTimeout(hovMove2, 10)
+  splashRight += 7
+  document.getElementById("splash").style.right = splashRight+"px"
+setTimeout(hovMove2, 10)
 }
-             
+           
 function hovMove2() {
-  if (splashRight > screen.width) { console.log("it's gone"); }
-  else { setTimeout(hovMove1, 10); }
+if (splashRight > screen.width) { console.log("it's gone"); }
+else { setTimeout(hovMove1, 10); }
 }
-
 
